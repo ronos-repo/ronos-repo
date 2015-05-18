adeb96641a87020c60c3d7b31aecbb87fa573433
ron23 <ron.r+github@outlook.com>
2015-05-18 09:47:00 -0700

    reverted playlistloader

--- plugin.video.playlistLoader/resources/lib/common.py
+++ plugin.video.playlistLoader/resources/lib/common.py
@@ -1,9 +1,7 @@
-import urllib, urllib2, os, io, xbmc, xbmcaddon, xbmcgui, json, re
+import urllib, urllib2, os, xbmc, xbmcaddon, xbmcgui, json, re
 
 AddonID = 'plugin.video.playlistLoader'
 Addon = xbmcaddon.Addon(AddonID)
-icon = Addon.getAddonInfo('icon')
-AddonName = Addon.getAddonInfo("name")
 
 def OpenURL(url, headers={}, user_data={}, justCookie=False):
 	if user_data:
@@ -24,59 +22,39 @@ def OpenURL(url, headers={}, user_data={}, justCookie=False):
 		else:
 			data = None
 	else:
-		data = response.read().replace("\r", "")
+		data = response.read()
 	
 	response.close()
 	return data
 
-def ReadFile(fileName):
-	try:
-		f = open(fileName,'r')
-		content = f.read().replace("\n\n", "\n")
-		f.close()
-	except:
-		content = ""
-
-	return content
-	
 def ReadList(fileName):
 	try:
-		with open(fileName, 'r') as handle:
-			content = json.load(handle)
-	except Exception as ex:
-		print ex
-		if os.path.isfile(fileName):
-			import shutil
-			shutil.copyfile(fileName, "{0}_bak.txt".format(fileName[:fileName.rfind('.')]))
-			xbmc.executebuiltin('Notification({0}, Cannot read file: "{1}". \nBackup createad, {2}, {3})'.format(AddonName, os.path.basename(fileName), 5000, icon))
-		content=[]
+		f = open(fileName,'r')
+		fileContent = f.read()
+		f.close()
+		content = json.loads(fileContent)
+	except:
+		content = []
 
 	return content
 
-def SaveList(filname, list):
+def SaveList(fileName, list):
 	try:
-		with io.open(filname, 'w', encoding='utf-8') as handle:
-			handle.write(unicode(json.dumps(list, indent=4, ensure_ascii=False)))
-		success = True
-	except Exception as ex:
-		print ex
-		success = False
-		
-	return success
+		with open(fileName, 'w') as outfile:
+			json.dump(list, outfile) 
+		outfile.close()
+		return True
+	except:
+		return False
 
 def OKmsg(title, line1, line2 = None, line3 = None):
 	dlg = xbmcgui.Dialog()
 	dlg.ok(title, line1, line2, line3)
 	
 def plx2list(url, group="Main"):
-	if url.find("http") >= 0:
-		response = OpenURL(url)
-	else:
-		response = ReadFile(url)
-	matches = re.compile("^background=(.*?)$",re.I+re.M+re.U+re.S).findall(response)
-	background = None if len(matches) < 1 else matches[0]
-	list = [{"background": background}]
+	response = OpenURL(url).replace("\r", "")
 	matches = re.compile('^type(.*?)#$',re.I+re.M+re.U+re.S).findall(response)
+	list = []
 	for match in matches:
 		item=re.compile('^(.*?)=(.*?)$',re.I+re.M+re.U+re.S).findall("type{0}".format(match))
 		item_data = {}
@@ -102,12 +80,8 @@ def flatten(list):
 '''
 
 def m3u2list(url):
-	if url.find("http") >= 0:
-		response = OpenURL(url)
-	else:
-		response = ReadFile(url)
-		
-	matches=re.compile('^#EXTINF:-?[0-9]*(.*?),(.*?)\n(.*?)$',re.I+re.M+re.U+re.S).findall(response)
+	response = OpenURL(url).replace("\r", "")
+	matches=re.compile('^#EXTINF:-1(.*?),(.*?)\n(.*?)$',re.I+re.M+re.U+re.S).findall(response)
 	li = []
 	for params, display_name, url in matches:
 		item_data = {"params": params, "display_name": display_name, "url": url}
@@ -121,21 +95,3 @@ def m3u2list(url):
 			item_data[field.strip().lower().replace('-', '_')] = value.strip()
 		list.append(item_data)
 	return list
-	
-def GetEncodeString(str):
-	try:
-		import chardet
-		str = str.decode(chardet.detect(str)["encoding"]).encode("utf-8")
-	except:
-		try:
-			str = str.encode("utf-8")
-		except:
-			pass
-	return str
-
-def DelFile(filname):
-	try:
-		if os.path.isfile(filname):
-			os.unlink(filname)
-	except Exception as e:
-		print e
