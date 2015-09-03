4a7669b8eb7f33a3c0e3d20ee38f71ab50958fc4
ron23 <ron.r+github@outlook.com>
2015-09-02 18:53:54 -0700

    update

---
+++ 1.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Git\bin\sh.exe" --login -i -c "git add -A"
+pause
--- run addon generator + push to git.bat
+++ run addon generator + push to git.bat
@@ -1,5 +1,5 @@
 python addons_xml_generator.py > AddonGeneratorResults.txt
-git add -A
-git commit -m "auto generator update"
-git push origin master
+:: git add -A
+:: git commit -m "auto generator update"
+:: git push origin master
 pause
